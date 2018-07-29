6bba920e7a60a23d49bf053804e7828184954023
Martin <m.becker@becker-software.de>
2018-07-29 18:44:31 +0200

    Upload Main Code

--- src/Api.php
+++ src/Api.php
@@ -6,7 +6,10 @@
 *  @author Martin Becker <martin.becker@becker-software.de>
 */
 class Api {
- 
+
+    protected $pass;
+    public $user;
+    public $url;
     /**
      * Constructor Function
      * 
@@ -15,6 +18,38 @@ class Api {
      */
     public function __construct($user,$pass)
     {
-        return "test";
+        $url = "https://restposten.ip-projects.de/api/hardware";
+
+        $key = hash ('sha256', env('ipprojects-api-password') , false );
+        $data = env('ipprojects-api-user') . $url ;
+        $digest = hash_hmac ('sha256', $data , $key , false );
+
+        $this->user = $user;
+        $this->pass = $pass;
+    }
+
+    protected function authorize($user,$pass,$url) {
+        $key = hash ('sha256', $pass , false );
+        $data = $user . "https://restposten.ip-projects.de/api/hardware";
+        $digest = hash_hmac ('sha256', $data , $key , false );
+        return $digest;
+    }
+    /**
+     * Request
+     * 
+     * @var url Submit the last part of the url after /api/{your-call}
+     */
+    public function request($url) {
+        $this->url = $url = 'https://restposten.ip-projects.de/api/'. $url;
+        $digest = $this->authorize($this->user,$this->pass,$this->url);
+        header('Content-Type: application/json');
+        $c = curl_init() ;
+        curl_setopt ($c , CURLOPT_URL , $this->url );
+        curl_setopt ($c , CURLOPT_HTTPHEADER , array ('AuthUser: '. $this->user , 'AuthDigest: ' . $digest ));
+        curl_setopt ($c , CURLOPT_RETURNTRANSFER , true );
+        $this->json = $json = curl_exec ($c);
+        $this->info = curl_getinfo($c);
+        curl_close ($c);
+        return $this->info;
     }
 }
