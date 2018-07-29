0d1ac722c050e492e49b9a379f56ebef5a713f1a
Martin <m.becker@becker-software.de>
2018-07-29 17:04:50 +0200

    Initial Commit

---
+++ src/Api.php
@@ -0,0 +1,20 @@
+<?php namespace MartinBecker\IPProjectsRestposten;
+
+/**
+*  Restposten API Wrapper
+*
+*  @author Martin Becker <martin.becker@becker-software.de>
+*/
+class Api {
+ 
+    /**
+     * Constructor Function
+     * 
+     * @var string $user Username
+     * @var string $pass Password
+     */
+    public function __construct($user,$pass)
+    {
+        return "test";
+    }
+}
